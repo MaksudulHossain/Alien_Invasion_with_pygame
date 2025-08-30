ea1770532f592badfe493d10fa4d309cbbbc1594
MaksudulHossain <jewelmaksud@gmail.com>
2025-08-30 07:12:10 -0700

    repopulate the fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -71,6 +71,9 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
         # print(len(self.bullets))
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+        if not self.aliens:
+            self.bullets.empty()
+            self._create_fleet()
 
     def _update_aliens(self):
         self._check_fleet_edges()
