380b062215f0965ddf77a7e52fd0cd98db48188e
MaksudulHossain <jewelmaksud@gmail.com>
2025-08-30 06:15:03 -0700

    create alien instances

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien 
 
 class AlienInvasion:
     def __init__(self):
@@ -15,6 +16,9 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
         # self.bg_color = (230,230,230)
 
     def run_game(self):
@@ -65,12 +69,17 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
         # print(len(self.bullets))
 
+    def _create_fleet(self):
+        alien = Alien(self)
+        self.aliens.add(alien)
+
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
 
         pygame.display.flip()
 
